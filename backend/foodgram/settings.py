07958fdce330c955e2bcb984165d2ad72c7750ad
awesky <asque.awesky@gmail.com>
2023-08-19 12:18:42 +0300

    fix: GitHub Workflow

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -88,8 +88,8 @@ DATABASES = {
             "DB_ENGINE", default="django.db.backends.postgresql"
         ),
         "NAME": os.getenv("DB_NAME", default="postgres"),
-        "USER": os.getenv("DB_USER", default="postgres"),
-        "PASSWORD": os.getenv("DB_PASSWORD", default="postgres"),
+        "USER": os.getenv("POSTGRES_USER", default="postgres"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD", default="postgres"),
         "HOST": os.getenv("DB_HOST", default="localhost"),
         "PORT": os.getenv("DB_PORT", default="5432"),
     }
